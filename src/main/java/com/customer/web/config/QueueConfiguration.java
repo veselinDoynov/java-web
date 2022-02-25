72d5fea4b087c9067cf202acc6c1bc8fe3b4c36e
Doinovi <veselin_doynov@abv.bg>
2022-02-25 16:03:49 +0200

    Add queue ... issue between jobs and tests

--- src/main/java/com/customer/web/config/QueueConfiguration.java
+++ src/main/java/com/customer/web/config/QueueConfiguration.java
@@ -5,9 +5,11 @@ import org.jobrunr.storage.InMemoryStorageProvider;
 import org.jobrunr.storage.StorageProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 
 @Configuration
+@PropertySource({"classpath:queue.properties"})
 public class QueueConfiguration {
 
     @Bean
